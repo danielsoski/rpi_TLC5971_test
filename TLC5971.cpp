b782d348b272e36228d79e132c8b52851c2b524d
Daniel Soski <danielsoski@gmail.com>
2018-06-23 16:09:21 -0700

    changing code structure to use c++ class

---
+++ TLC5971.cpp
@@ -0,0 +1,122 @@
+// TLC5971.cpp
+
+#include "TLC5971.h"
+#include <wiringPi.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+TLC5971::TLC5971(int num, int pinSCKI, int pinSDTI)
+{
+    cout << "Parametrized Constructor called" << endl;
+    this->num = num;
+    this->pinSCKI = pinSCKI;
+    this->pinSDTI = pinSDTI;
+    wiringPiSetup();
+    pinMode(pinSCKI, OUTPUT);
+    pinMode(pinSDTI, OUTPUT);
+}
+
+void TLC5971::sleep_setup() {
+  nanosleep(this->&tsetup, NULL);
+}
+
+void TLC5971::sleep_width() {
+  nanosleep(this->&twidth, NULL);
+}
+
+void TLC5971::sleep_hold() {
+  nanosleep(this->&thold, NULL);
+}
+
+int TLC5971::send_bit(int val)
+{
+  if (val != 1 && val != 0) {
+    return 0;
+  }
+
+  digitalWrite(this->pinSDTI, val);
+  this->sleep_setup();
+  digitalWrite(this->pinSCKI, HIGH);
+  this->sleep_width();
+  digitalWrite(this->pinSCKI, LOW);
+  this->sleep_hold();
+  digitalWrite(this->pinSDTI, LOW);
+
+  return 1;
+}
+
+void TLC5971::send_bits(int len, uint16_t val) {
+  int i;
+  for (i=len-1; i>=0; i--) {
+    this->send_bit((val >> i) & 0x1);
+  }
+}
+
+void TLC5971::send_6bits(uint8_t val) {
+  this->send_bits(6, val);
+}
+
+void TLC5971::send_7bits(uint8_t val) {
+  this->send_bits(7, val);
+}
+
+void TLC5971::send_16bits(uint16_t val) {
+  this->send_bits(16, val);
+}
+
+void TLC5971::printConfig()
+{
+  cout << "num: " << this->num << endl;
+  cout << "pinSCKI: " << this->pinSCKI << endl;
+  cout << "pinSDTI: " << this->pinSDTI << endl;
+}
+
+void TLC5971::write()
+{
+  send_6bits(CMD_WRITE);
+
+  send_bit(OUTTMG_FALL);
+  send_bit(EXTGCK_INT);
+  send_bit(TMGRST_DIS);
+  send_bit(DSPRPT_DIS);
+  send_bit(BLANK_ON);
+
+  send_7bits(BC_1MA); // BC BLUE 0-3
+  send_7bits(BC_1MA); // BC GREEN 0-3
+  send_7bits(BC_1MA); // BC RED 0-3
+
+  send_16bits(0); // GS BLUE 3
+  send_16bits(0); // GS GREEN 3
+  send_16bits(0); // GS RED 3
+
+  send_16bits(0); // GS BLUE 2
+  send_16bits(0); // GS GREEN 2
+  send_16bits(0); // GS RED 2
+
+  send_16bits(GS_100); // GS BLUE 1
+  send_16bits(GS_100); // GS GREEN 1
+  send_16bits(GS_100); // GS RED 1
+
+  send_16bits(GS_100); // GS BLUE 0
+  send_16bits(GS_100); // GS GREEN 0
+  send_16bits(GS_100); // GS RED 0
+}
+
+void TLC5971::set_blank(bool val)
+{
+  if (val == true) {
+    this->config_blank = BLANK_ON;
+  }
+  else {
+    this->config_blank = BLANK_OFF;
+  }
+}
+
+void TLC5971::set_8bit_color(int channel, uint8_t r, uint8_t, g, uint8_t b)
+{
+    if (channel < 0 || channel > 3) return;
+    int r_scaled = (int)r * GS_100 / 257;
+
+    cout << "Geek id is: " << id;
+}
---
+++ TLC5971.h
@@ -0,0 +1,76 @@
+// TLC5971.h
+#include <stdint.h>
+#include <time.h>
+
+#define T_SCALE 10000
+
+#define CMD_WRITE 0x25
+#define OUTTMG_FALL 0
+#define OUTTMG_RISE 1
+#define EXTGCK_INT 1
+#define TMGRST_DIS 0
+#define TMGRST_EN 1
+#define DSPRPT_DIS 0
+#define DSPRPT_EN 1
+#define BLANK_ON 0
+#define BLANK_OFF 1
+
+#define BC_1MA  2
+#define BC_10MA 21
+#define BC_20MA 42
+#define BC_30MA 63
+#define BC_40MA 65
+#define BC_50MA 106
+#define BC_60MA 127
+
+#define GS_10  6553
+#define GS_20  13107
+#define GS_30  19660
+#define GS_40  26214
+#define GS_50  32767
+#define GS_60  39321
+#define GS_70  45874
+#define GS_80  52428
+#define GS_90  58981
+#define GS_100 65535
+
+class TLC5971
+{
+  private:
+  int num;
+  int pinSCKI;
+  int pinSDTI;
+
+  struct timespec tsetup = {.tv_sec=0, .tv_nsec=5L*T_SCALE};
+  struct timespec thold = {.tv_sec=0, .tv_nsec=3L*T_SCALE};
+  struct timespec twidth = {.tv_sec=0, .tv_nsec=10L*T_SCALE};
+
+  bool config_outtmg = OUTTMG_FALL;
+  bool config_extgck = EXTGCK_INT;
+  bool config_tmgrst = TMGRST_DIS;
+  bool config_dsprpt = DSPRPT_DIS;
+  bool config_blank = BLANK_OFF;
+
+  uint8_t config_bc_r = 0;
+  uint8_t config_bc_g = 0;
+  uint8_t config_bc_b = 0;
+
+  uint16_t config_gs_r[4] = {0};
+  uint16_t config_gs_g[4] = {0};
+  uint16_t config_gs_b[4] = {0};
+
+  void sleep_setup();
+  void sleep_width();
+  void sleep_hold();
+  int send_bit(int val);
+  void send_bits(int len, uint16_t val);
+  void send_6bits(uint8_t val);
+  void send_7bits(uint8_t val);
+  void send_16bits(uint16_t val);
+
+  public:
+  void printConfig();
+  void write();
+  void set_blank(bool val);
+  void set_8bit_color(int channel, uint8_t r, uint8_t, g, uint8_t b);
+};
