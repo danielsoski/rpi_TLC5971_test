85cc97a24909c1db5ecc5e93e78f05928972bc66
Daniel Soski <danielsoski@gmail.com>
2018-06-23 22:50:27 -0700

    fixing printConfig and write

--- TLC5971.cpp
+++ TLC5971.cpp
@@ -76,15 +76,15 @@ void TLC5971::printConfig()
   cout << "config_dsprpt: " << this->config_dsprpt << endl;
   cout << "config_blank: " << this->config_blank << endl;
 
-  cout << "config_bc_r: " << (unsigned int)this->config_bc_r << endl;
-  cout << "config_bc_g: " << (unsigned int)this->config_bc_g << endl;
   cout << "config_bc_b: " << (unsigned int)this->config_bc_b << endl;
+  cout << "config_bc_g: " << (unsigned int)this->config_bc_g << endl;
+  cout << "config_bc_r: " << (unsigned int)this->config_bc_r << endl;
 
   int i;
-  for (i=0; i<4; i++) {
-    cout << "config_gs_r[" << i << "]: " << this->config_gs_r[i] << endl;
-    cout << "config_gs_g[" << i << "]: " << this->config_gs_g[i] << endl;
+  for (i=3; i>=0; i--) {
     cout << "config_gs_b[" << i << "]: " << this->config_gs_b[i] << endl;
+    cout << "config_gs_g[" << i << "]: " << this->config_gs_g[i] << endl;
+    cout << "config_gs_r[" << i << "]: " << this->config_gs_r[i] << endl;
   }
 }
 
@@ -92,31 +92,22 @@ void TLC5971::write()
 {
   send_6bits(CMD_WRITE);
 
-  send_bit(OUTTMG_FALL);
-  send_bit(EXTGCK_INT);
-  send_bit(TMGRST_DIS);
-  send_bit(DSPRPT_DIS);
-  send_bit(BLANK_ON);
+  send_bit(this->config_outtmg);
+  send_bit(this->config_extgck);
+  send_bit(this->config_tmgrst);
+  send_bit(this->config_dsprpt);
+  send_bit(this->config_blank);
 
-  send_7bits(BC_1MA); // BC BLUE 0-3
-  send_7bits(BC_1MA); // BC GREEN 0-3
-  send_7bits(BC_1MA); // BC RED 0-3
+  send_7bits(this->config_bc_b); // BC BLUE 0-3
+  send_7bits(this->config_bc_g); // BC GREEN 0-3
+  send_7bits(this->config_bc_r); // BC RED 0-3
 
-  send_16bits(0); // GS BLUE 3
-  send_16bits(0); // GS GREEN 3
-  send_16bits(0); // GS RED 3
-
-  send_16bits(0); // GS BLUE 2
-  send_16bits(0); // GS GREEN 2
-  send_16bits(0); // GS RED 2
-
-  send_16bits(GS_100); // GS BLUE 1
-  send_16bits(GS_100); // GS GREEN 1
-  send_16bits(GS_100); // GS RED 1
-
-  send_16bits(GS_100); // GS BLUE 0
-  send_16bits(GS_100); // GS GREEN 0
-  send_16bits(GS_100); // GS RED 0
+  int i;
+  for (i=3; i>=0; i--) {
+    send_16bits(this->config_gs_b[i]); // GS BLUE
+    send_16bits(this->config_gs_g[i]); // GS GREEN
+    send_16bits(this->config_gs_r[i]); // GS RED
+  }
 }
 
 void TLC5971::set_blank(bool val)
