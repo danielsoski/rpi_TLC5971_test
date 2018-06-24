2fa67a42fd172dde0a0cebfa032eb10f11adc1a6
Daniel Soski <danielsoski@gmail.com>
2018-06-23 22:37:24 -0700

    more useful main

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 int main (void)
 {
   TLC5971 leds(0, PIN_SCKI, PIN_SDTI);
+  leds.set_current_ma(20);
+  leds.set_8bit_color(0, 255, 197, 143); // 2600K - 40W Tungsten
+  leds.set_8bit_color(1, 255, 197, 143); // 2600K - 40W Tungsten
   leds.printConfig();
 
   return 0;
